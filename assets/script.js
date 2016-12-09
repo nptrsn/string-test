df2b7322aa0a60c9854faf76e1f20991aa73e85a
nptrsn <nick@hindiemedia.com>
2016-12-09 13:54:46 -0800

    committing unfinished dynamic noshed dynamic note string image function

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,50 @@
+// Setting Strings and Notes
+var strings = [
+  "Low E", "A", "D", "G", "B", "E"
+];
 var notes = [
 	"A","A#","Bb", "B","C","C#","Db","D","D#","Eb","E","F","F#","Gb","G","G#","Ab"
 ];
-var strings = [
-	"Low E", "A"
-  // , "D", "G", "B", "E"
-];
 
-function getNote() {
-   var theNote = notes[Math.floor(Math.random() * notes.length)];
-   $('.note').html(theNote);
-}
-
-function getString() {
-	var theString = strings[Math.floor(Math.random() * strings.length)];
-	$('.string').html(theString);
-}
-
-setInterval(function() {
+function BigParentFunction() {
+  var ranNum = Math.floor(Math.random()*73);
+  function getNote() {
+    var newNote = notes[Math.floor(ranNum / notes.length)];
+    $('.note').html(newNote);
+  }
+  function getString() {
+    var newString = strings[Math.floor(ranNum / strings.length)];
+    $('.string').html(newString + " string");
+  }
+  function getImage() {
+    $('.cheat-sheet').attr('src','assets/img/' + ranNum + '.png');
+  }
   getNote();
   getString();
-}, 6000);
+  getImage();
+}
 
+
+
+// var theNote = notes[randomValue * notes.length];
+// console.log(theNote);
+
+//   $('.note').html(theNote);
+
+  
+// function getString() {
+//   var theString = strings[randomValue * strings.length];
+//   $('.string').html(theString + " String");
+// }
+
+
+setInterval(function() {
+  BigParentFunction();
+
+}, 5000);
+
+
+// Button Functions
 function buttonClick(){
 	$('.strings-button').toggleClass('active');
 	$('.string').toggleClass('hide');
@@ -31,3 +55,9 @@ function cheatSheet(){
   $('.cheat-toggle').toggleClass('deactivated');
   $('.cheat-sheet').toggleClass('hide');
 }
+
+// Note Sounds //
+
+function playVid() { 
+    $('.sound')[0].play(); 
+} 
