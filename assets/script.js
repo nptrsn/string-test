bf1f66b4baaa556e33879528f8986525ccd229d4
nptrsn <nick@hindiemedia.com>
2016-12-09 15:36:18 -0800

    working version, need to clean up comments

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,71 @@
 // Setting Strings and Notes
 var strings = [
-  "Low E", "A", "D", "G", "B", "E"
+  "Low E", "A", "D", "G", "B", "High E"
 ];
 var notes = [
-	"A","A#","Bb", "B","C","C#","Db","D","D#","Eb","E","F","F#","Gb","G","G#","Ab"
+  // Splittin Array Lines by String, 12 notes over 6 strings
+	"F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E",
+  "A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A",
+  "D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D",
+  "G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G",
+  "C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B",
+  "F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E"
 ];
 
 function BigParentFunction() {
   var ranNum = Math.floor(Math.random()*73);
-  function getNote() {
-    var newNote = notes[Math.floor(ranNum / notes.length)];
-    $('.note').html(newNote);
-  }
+
   function getString() {
-    var newString = strings[Math.floor(ranNum / strings.length)];
+    console.log("number " + ranNum);
+    console.log("this is on string #" + (Math.floor(ranNum / 12)+1) + " or the " + strings[Math.floor(ranNum / 12)] + " string");
+    var newString = strings[Math.floor(ranNum / 12)];
     $('.string').html(newString + " string");
+  }
+  
+  function getNote() {
+    var newNote = notes[ranNum-1];
+    $('.note').html(newNote);
+    // Conditionals for flats & sharps, since they create file path errors
+    if (newNote == "F#/Gb") {
+      $('.sound').attr('src','assets/audio/Fsharp.mp3');
+    }
+    else if (newNote == "G#/Ab") {
+      $('.sound').attr('src','assets/audio/Gsharp.mp3');
+    }
+    else if (newNote == "A#/Bb") {
+      $('.sound').attr('src','assets/audio/Asharp.mp3');
+    }
+    else if (newNote == "C#/Db") {
+      $('.sound').attr('src','assets/audio/Csharp.mp3');
+    }
+    else if (newNote == "D#/Eb") {
+      $('.sound').attr('src','assets/audio/Dsharp.mp3');
+    }
+    // Major notes
+    else {
+      $('.sound').attr('src','assets/audio/' + newNote + '.mp3');
+    }
+    console.log("the note is " + newNote);
+    console.log("/////////////");
   }
   function getImage() {
     $('.cheat-sheet').attr('src','assets/img/' + ranNum + '.png');
   }
-  getNote();
+ 
   getString();
   getImage();
+  getNote();
+
 }
-
-
-
-// var theNote = notes[randomValue * notes.length];
-// console.log(theNote);
-
-//   $('.note').html(theNote);
-
-  
-// function getString() {
-//   var theString = strings[randomValue * strings.length];
-//   $('.string').html(theString + " String");
-// }
-
 
 setInterval(function() {
   BigParentFunction();
-
-}, 5000);
-
+}, 8000);
 
 // Button Functions
-function buttonClick(){
-	$('.strings-button').toggleClass('active');
-	$('.string').toggleClass('hide');
-	$('.note').toggleClass('margin');
-}
 
 function cheatSheet(){
   $('.cheat-toggle').toggleClass('deactivated');
   $('.cheat-sheet').toggleClass('hide');
 }
 
-// Note Sounds //
-
-function playVid() { 
-    $('.sound')[0].play(); 
-} 
